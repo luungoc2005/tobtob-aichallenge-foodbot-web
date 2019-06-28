e73718a3f09077efd45c8f0dd75b0b891e1d461b
luungoc2005 <luungoc2005@gmail.com>
2019-06-29 06:09:21 +0700

    Initial commit

---
+++ src/components/PageLayout/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import { Layout, Header } from 'antd';
+
+export const PageLayout = () => <Layout>
+</Layout>
+
+export default PageLayout;
---
+++ src/containers/HomePage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default HomePage;
